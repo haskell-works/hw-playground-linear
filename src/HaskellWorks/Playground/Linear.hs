46d0525b73d2ad4326b3e5d866b0924e2be09117
John Ky <newhoggy@gmail.com>
2019-12-03 17:45:39 +1100

    Stuff

--- src/HaskellWorks/Playground/Linear.hs
+++ src/HaskellWorks/Playground/Linear.hs
@@ -8,3 +8,21 @@ variance m = x <> ((m - asRow rdMean) ** 2)
   where (rdMean, _) = meanCov m
         r = rows m
         x = konst (1 / fromIntegral r) (1, r)
+
+errors :: Matrix Double -> Matrix Double
+errors m = (m - asRow rdMean)
+  where (rdMean, _) = meanCov m
+        r           = rows m
+
+correlation :: Matrix Double -> Matrix Double
+correlation m = build (c, c) mkCorrelation
+  where stddev  = flatten (sqrt (variance m)) :: Vector Double
+        (_, c)  = size m
+        err     = errors m
+        mkCorrelation :: Double -> Double -> Double
+        mkCorrelation di dj = sumElements (x * y) / fromIntegral r / (stddev ! i) / (stddev ! j)
+          where i = round di :: Int
+                j = round dj :: Int
+                (r, _) = size x
+                x = err ¿ [i]
+                y = err ¿ [j]
